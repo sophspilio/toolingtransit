4efe0677ec19b8c6b909f53ae8ca552f95c34583
sophspilio <sophspilio@gmail.com>
2023-11-09 13:25:00 -0500

    updates file locations

--- notebooks/routesforGIS.R
+++ notebooks/routesforGIS.R
@@ -11,26 +11,18 @@ GTFS_path <- file.path ("Z:",
                         "GTFS")
 
 
-ARTzip <- "2023-02_Arlington.zip"
-CUEzip <- "2023-02_CUE.zip"
-DASHzip <- "2023-02_DASH.zip"
-FFXzip <- "2023-02_Fairfax_Connector.zip"
-LCTzip <- "2023-02_Loudoun.zip"
-PRTCzip <- "2023-02_OmniRide_PRTC.zip"
-VREzip <- "2023-02_VRE.zip"
-Metrobuszip <- "2023-02_Metrobus.zip"
-Metrorailzip <- "2023-02_Metrorail.zip"
+ARTzip <- "2023-07_Arlington.zip"
+CUEzip <- "2023-07_CUE.zip"
+DASHzip <- "2023-07_DASH.zip"
+FFXzip <- "2023-07_Fairfax_Connector.zip"
+LCTzip <- "2023-07_Loudoun.zip"
+PRTCzip <- "2023-07_OmniRide_PRTC.zip"
+VREzip <- "2023-07_VRE.zip"
+Metrobuszip <- "2023-07_Metrobus.zip"
+Metrorailzip <- "2023-07_Metrorail.zip"
 
 
-routes <- function(GTFSzip){
-  GTFS <- read_gtfs(file.path(GTFS_path, GTFSzip)) %>% gtfs_as_sf(.)
 
-  routes <- inner_join(GTFS$trips, GTFS$routes) %>%
-    select(route_id, shape_id) %>% inner_join(.,GTFS$shapes) %>% st_sf() %>%
-    group_by(route_id) %>% summarize(n = n()) %>% select(-n) %>%
-    mutate(GTFS_date = sub("_.*", "", GTFSzip))
-  return(routes)
-}
 
 routes <- function(GTFSzip){
   GTFS <- read_gtfs(file.path(GTFS_path, GTFSzip))
@@ -48,7 +40,7 @@ routes <- function(GTFSzip){
 }
 
 
-file <- "Z:/NVTC General/Projects and Programs/Transit Resource Center (TRC)/Data/GIS Data/Spatial Repo/R Bus Routes + Stops"
+file <- "Z:/NVTC General/Projects and Programs/Transit Resource Center (TRC)/Data/GIS Data/R Bus Routes + Stops"
 
 st_write(routes(ARTzip), file.path(file, "ARTRoutes.shp"), delete_dsn = TRUE)
 st_write(routes(PRTCzip), file.path(file, "PRTCRoutes.shp"), delete_dsn = TRUE)
@@ -60,6 +52,14 @@ st_write(routes(LCTzip), file.path(file, "LCTRoutes.shp"), delete_dsn = TRUE)
 st_write(routes(Metrorailzip), file.path(file, "Metrorail.shp"), delete_dsn = TRUE)
 st_write(routes(Metrobuszip), file.path(file, "Metrobus.shp"), delete_dsn = TRUE)
 
-
-
+rbind(
+  routes(ARTzip) %>% mutate(Agency = "ART", Mode = "Bus"),
+  routes(PRTCzip) %>% mutate(Agency = "OmniRide", Mode = "Bus"),
+  routes(VREzip) %>% mutate(Agency = "VRE", Mode = "Commuter Rail"),
+  routes(CUEzip) %>% mutate(Agency = "CUE", Mode = "Bus"),
+  routes(DASHzip) %>% mutate(Agency = "DASH", Mode = "Bus"),
+  routes(FFXzip) %>% mutate(Agency = "Fairfax Connector", Mode = "Bus"),
+  routes(Metrorailzip) %>% mutate(Agency = "WMATA", Mode = "Heavy Rail"),
+  routes(Metrobuszip) %>% mutate(Agency = "WMATA", Mode = "Bus")
+) %>% st_write(file.path(file, "AllTransitRoutes.shp"), delete_dsn = TRUE)
 
